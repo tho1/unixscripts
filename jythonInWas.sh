4a1b3bcfc928d0cae9304e514ed99d2ad81201b4
= <thomaswbho@gmail.com>
2014-03-25 00:03:34 -0400

    added jythonInWas.sh

---
+++ jythonInWas.sh
@@ -0,0 +1,7 @@
+!/bin/bash
+
+#This uses the jython that is built into the Websphere app for testing.
+#Might need to set the path in order to init it.
+
+/opt/IBM/WebSphere80/AppServer/bin/wsadmin.sh -lang jython
+
